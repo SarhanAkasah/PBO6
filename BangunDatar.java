792b75877425120d1c8c490a38434aef4aa7c72c
M. Sarhan Akasah <114375641+SarhanAkasah@users.noreply.github.com>
2022-11-12 22:18:14 +0700

    Add files via upload

---
+++ BangunDatar.java
@@ -0,0 +1,14 @@
+
+public abstract class BangunDatar {
+    String warna;
+    
+    String getwarna() {
+        return warna;
+    }
+
+    void setWarna(String warna) {
+        this.warna = warna;
+    }
+
+    abstract float getLuas();
+}
---
+++ Laptop/LaptopUser.java
@@ -0,0 +1,28 @@
+//M.Sarhan akasah
+//20210040210
+//TI21G
+package Laptop;
+
+public class LaptopUser {
+    private final Laptop laptop;
+
+    public LaptopUser(Laptop laptop){
+        this.laptop=laptop;
+    }
+
+    void turnOnLaptop(){
+        this.laptop.powerOn();
+    }
+
+    void turnOffLaptop(){
+        this.laptop.powerOff();
+    }
+
+    void makeLaptopLouder(){
+        this.laptop.volumeUp();
+    }
+
+    void makeLaptopSilence(){
+        this.laptop.volumeDown();
+    }
+}
